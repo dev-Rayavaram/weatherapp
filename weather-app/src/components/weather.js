672ff1f3c5888f016ea0c074f2435987af6d1b1a
Student_M20 <student_m20@PS-DAL-M20.local>
2020-04-06 17:50:53 -0500

    added axios get methods to retrieve data from weather app

--- weather-app/src/components/weather.js
+++ weather-app/src/components/weather.js
@@ -3,44 +3,21 @@ import {Route,Switch,Link,BrowserRouter as Router} from 'react-router-dom'
 import axios from 'axios';
 
 
-export default class Weather extends Component
-    {
-      constructor(props){
-        super(props);
-        this.state={
-          value:'Plano',
-          style:{
-                  'textDecoration':'none',
-                  backgroundColor:'#005689',
-                  color:'white',
-                  border:'2px solid white',
-                  'fontFamily':'Jacques Francois',
-                  'fontSize': '30px'
-          }
-        }
- 
+export default class Weather extends Component{
+    constructor(props){
+      super(props);
+      this.state={
+          onedayData:[],
+          fiveDayData:[]
       }
-      onMouseClick=(place)=>{
+    }
+     onMouseClick=(place)=>{
 
-       }
- // let apiKey=process.env.REACT_APP_API_KEY
-  //api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=0f13e039816ac1a8397eb8b68851cb9d
-  // axios.get(`api.openweathermap.org/data/2.5/weather?APPID=${apiKey}`)
-  //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={YOUR API KEY}
-  componentDidMount() {
-   
-     let apiKey=process.env.REACT_APP_API_KEY
-      axios.get(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/locationKey?apikey=${apiKey}`)
-     .then(res => {
-       const results = res.data.response.results;
-        console.log(results)
-     })
-     .catch(error => {
-       console.log('there is an eror', error)
-     })
- 
-   }
- 
+    }
+    // let apiKey=process.env.REACT_APP_API_KEY
+      //api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=0f13e039816ac1a8397eb8b68851cb9d
+      // axios.get(`api.openweathermap.org/data/2.5/weather?APPID=${apiKey}`)
+      //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={YOUR API KEY}
       render(){
         return (
  
@@ -49,10 +26,10 @@ export default class Weather extends Component
             <nav>
                 <ul>
                   <li>
-                  <Link to="/day"  style={this.state.style} onClick={()=>this.onMouseClick('News')} >5 day forecast</Link>
+                  <Link to="/day"  style={this.props.style} onClick={()=>this.onMouseClick('5day')} >5 day forecast</Link>
                 </li>
                 <li>
-                  <Link to="/"  style={this.state.style} onClick={()=>this.onMouseClick('Opinion')}>Hourly forecast</Link>
+                  <Link to="/"  style={this.props.style} onClick={()=>this.onMouseClick('oneday')}>Hourly forecast</Link>
                 </li>
                  </ul>
             </nav>
@@ -71,6 +48,32 @@ export default class Weather extends Component
     } 
 
       class FiveDays extends Component {
+        constructor(props){
+          super(props);
+          this.state={
+              fiveDayData:[]
+          }
+        }
+        componentDidMount() {
+      
+          let apiKey=process.env.REACT_APP_API_KEY
+            axios.get(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/75094?apikey=${apiKey}`)
+          .then(res => {
+            const results = res.data;
+              console.log(results)
+              let items=[]
+              for(let i=0; i<results.length;i++){
+                //  console.log(items[i].sectionName)
+                items.push(results[i]);
+                }
+                this.setState({fiveDayData:items});
+
+          })
+          .catch(error => {
+            console.log('there is an eror', error)
+          })
+      
+        }       
          render(){
           return(
             <div className='body'>
@@ -80,17 +83,37 @@ export default class Weather extends Component
 
           );
         }
- }
+    }
 
-          class OneDay extends Component {
-            render(){
-              return(
-                <div className='body'>
-                <span> Inside oneday</span>
+    class OneDay extends Component {
+      constructor(props){
+        super(props);
+        this.state={
+            onedayData:[]
+        }
+      }
+      componentDidMount() {
+      
+        let apiKey=process.env.REACT_APP_API_KEY
+          axios.get(`http://dataservice.accuweather.com/forecasts/v1/daily/1day/75094?apikey=${apiKey}`)
+        .then(res => {
+          const results = res.data;
+          console.log(results)
+          this.setState({onedayData:results});
+        })
+        .catch(error => {
+          console.log('there is an eror', error)
+        })
+    
+      }       
+      render(){
+            return(
+              <div className='body'>
+              <span> Inside oneday</span>
 
-                </div>
+              </div>
 
-              );
+            );
             
 
             }
