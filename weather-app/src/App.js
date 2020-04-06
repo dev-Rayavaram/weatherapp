fc954aca43f39f541971970aafe24fbfc53cb97f
Student_M20 <student_m20@PS-DAL-M20.local>
2020-04-06 13:26:36 -0500

    added styles for footer, header,main

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import Footer from './components/footer.js'
+import Header from './components/header.js'
+import Main from './components/main.js'
+
 
 function App() {
   return (
     <div className="App">
-     
+     <Header/>
+     <Main/>
+     <Footer/>
     </div>
   );
 }
