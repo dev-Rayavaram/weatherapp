e75d6309eebeae2cd0acc547366a05ac9ff4fba0
Student_M20 <student_m20@PS-DAL-M20.local>
2020-04-06 13:43:45 -0500

    fixed errors for App.js

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './App.css';
 import Footer from './components/footer.js'
 import Header from './components/header.js'
 import Main from './components/main.js'
 
-
-function App() {
-  return (
-    <div className="App">
-     <Header/>
-     <Main/>
-     <Footer/>
-    </div>
-  );
+class App extends Component {
+    render() {
+      return (
+        <div className="App">
+          <Header/>
+          <Main/>
+          <Footer/>
+        </div>
+      )
+  }
 }
-
 export default App;
---
+++ weather-app/src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+     HI
+    </div>
+  );
+}
+
+export default Header;
