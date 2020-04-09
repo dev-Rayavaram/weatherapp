edeaf8435810de1762dd37eb1b3a417b9930fdb0
Student_M20 <student_m20@PS-DAL-M20.local>
2020-04-08 21:29:42 -0500

    filename changed for header component

--- weather-app/src/components/header.js
+++ weather-app/src/components/header.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Header() {
   return (
-    <div>
-     HI
+    <div className='header'>
+     <h1 > Welcome to the Weather App. <br></br>Please Click on One of the Following Links to Continue</h1>
     </div>
   );
 }
